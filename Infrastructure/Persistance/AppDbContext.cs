d422ec4722ad469a29e7eba6dbdfd456b1dd3835
alkanfatih <alkanfatih@hotmail.com.tr>
2025-11-20 20:03:08 +0300

    Db Context

---
+++ Infrastructure/Persistance/AppDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Commons;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Infrastructure.Persistance
+{
+    public class AppDbContext : IdentityDbContext<Customer, IdentityRole<int>, int>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // DbSet Tanımları
+        public DbSet<Product> Products => Set<Product>();
+        public DbSet<Category> Categories => Set<Category>();
+        public DbSet<ProductImage> ProductImages => Set<ProductImage>();
+        public DbSet<Address> Addresses => Set<Address>();
+        public DbSet<Order> Orders => Set<Order>();
+        public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+        public DbSet<ProductDetail> ProductDetails => Set<ProductDetail>();
+        public DbSet<ShoppingCart> ShoppingCarts => Set<ShoppingCart>();
+        public DbSet<CartItem> CartItems => Set<CartItem>();
+        public DbSet<AgencyApplication> AgencyApplications => Set<AgencyApplication>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //Configurations yapılarını uygula
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
+}
